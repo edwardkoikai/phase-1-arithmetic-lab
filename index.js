0b1d69f2207a2ba70adb99d2fb4be9d74855df41
Edward Koikai <edwardkoikai@gmail.com>
2024-03-21 05:27:51 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+//Task Two
+let random = Math.floor(Math.random() * Number.MAX_VALUE) + 1;
 
-// Write your code here
+// Task 3
+
+const num3 = 14;
+const num4 = 5;
+let mod = num3 % num4;
+
+// Task 4
+let max = Math.max(2,8,20,15,6);
